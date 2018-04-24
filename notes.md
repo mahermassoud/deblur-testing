de1f85a0972a0628342d7de7aff53fad7873fc22
massoudTheDude <mahermassoud@gmail.com>
2018-04-24 13:41:22 -0700

    Commented out superfluous code

--- notes.md
+++ notes.md
@@ -1,7 +1,28 @@
 # High priority
-- aggregate script
-- integration tests
+1. Make dotfile for bash config, use miniconda
+2. Put it on cluster
+3. Make interactive job
+4. simple qsub / pbs script
+5. how to request 3 processors and 24gb of memory, walltime of 2 hr (kill after 2 hr)
+    - know how to kill a job thats running or in the queue
+6. how to automaticaly setup a temp directory on panfs
+    - when done move it to home, delete
+7. Run unit tests in command line    
+8. Write script to see if num_cores param makes a difference!
+
+
+
+# Low priority
+- do eg.s in python console format
 - try with normalized data, non euclidian, divide vector by sum of vector
+- get rid of percents business
+- Fix Demux test in methods by using small samples
+- look @ calculating trim length
+- input validity checks
+- make clps_df flow better
+- lint
+- make -o default to . instead of requiring
+- get rid of docstrings from click commands and improve interface, --help
 - more metrics!! eg. seq depth
      -Look @ distribution of difference-per-featureplot taxa that got dropped out
      -look @ distribution of top collapsed features
@@ -12,21 +33,8 @@
      -Straight up jac/bc/mantel between biom tables
      -sequencing depth
 
-# Low priority
-- do eg.s in python console format
-- get rid of percents business
-- Fix Demux test in methods by using small samples
-- look @ calculating trim length
-- faster test for pre_trims in test_scripts.py
-- input validity checks
-- make clps_df flow better
-- lint
-- make -o default to . instead of requiring
-- get rid of docstrings from click commands and improve interface, --help
-
 # Can't do yet
 - test against different environments eg. fecal, skin, soil
-
 
 # Ideas
 - append sample name in pre_post
@@ -36,7 +44,7 @@
 
 # Meeting notes 4/19/18
 - make a dotfile github bash_profile, copy dans
-- depdencies, download miniconda, make conda inv
+- depenencies, download miniconda, make conda inv
     1) make environment
     2) make interactive job, understand when in and out of the job
     3) qsub/pbs script, something simple like submitting hostname
--- test/test_methods.py
+++ test/test_methods.py
@@ -13,38 +13,39 @@ from pandas.util.testing import assert_frame_equal
 
 NUM_CORES = 4
 
-class TestImport(TestCase):
-
-    def setUp(self):
-        print("Importing metadata for expected")
-        self.exp_barcode_metadata = \
-            Metadata.load("data/mock-3/sample-metadata.tsv")
-
-        self.exp_demux = Artifact.load("data/mock-3/exp_demux.qza")
-        self.exp_out = [self.exp_demux, self.exp_barcode_metadata]
-        self.working_dir_fp = "data/mock-3"
-
-    def test_import_dataset(self):
-        obs = import_dataset(self.working_dir_fp, "BarcodeSequence")
-        for i, entry in enumerate(obs):
-            try:
-                self.assertEqual(entry, self.exp_out[i])
-            except AssertionError as e:
-                # TODO fix this
-                if("uuid" in str(e)): # uuid is always unique so we expect this
-                    continue
-                else:
-                    self.fail(str(e))
-
-class TestDeblur(TestCase):
-
-    def setUp(self):
-        self.exp_demux = Artifact.load("data/mock-3/exp_demux.qza")
-        self.exp_deblurred = Artifact.load("data/mock-3/deblurred_150nt.qza")
-        self.exp_deblurred_pt = Artifact.load("data/mock-3/deblurred_100nt_pt.qza")
-        self.num_parallel = NUM_CORES
-
-    def test_establish_dataset(self):
+# Commented out because it takes a while
+#class TestImport(TestCase):
+#
+#    def setUp(self):
+#        print("Importing metadata for expected")
+#        self.exp_barcode_metadata = \
+#            Metadata.load("data/mock-3/sample-metadata.tsv")
+#
+#        self.exp_demux = Artifact.load("data/mock-3/exp_demux.qza")
+#        self.exp_out = [self.exp_demux, self.exp_barcode_metadata]
+#        self.working_dir_fp = "data/mock-3"
+#
+#    def test_import_dataset(self):
+#        obs = import_dataset(self.working_dir_fp, "BarcodeSequence")
+#        for i, entry in enumerate(obs):
+#            try:
+#                self.assertEqual(entry, self.exp_out[i])
+#            except AssertionError as e:
+#                # TODO fix this
+#                if("uuid" in str(e)): # uuid is always unique so we expect this
+#                    continue
+#                else:
+#                    self.fail(str(e))
+#
+#class TestDeblur(TestCase):
+#
+#    def setUp(self):
+#        self.exp_demux = Artifact.load("data/mock-3/exp_demux.qza")
+#        self.exp_deblurred = Artifact.load("data/mock-3/deblurred_150nt.qza")
+#        self.exp_deblurred_pt = Artifact.load("data/mock-3/deblurred_100nt_pt.qza")
+#        self.num_parallel = NUM_CORES
+#
+#    def test_establish_dataset(self):
         obs = do_deblur(self.exp_demux, 150)
         self.assertEqual(self.exp_deblurred.view(biom.Table), obs.view(biom.Table))
 
@@ -269,40 +270,40 @@ class TestGetCountData(TestCase):
         assert_frame_equal(self.exp_rps, obs_rps, check_dtype=False)
 
 # Tests for methods specific to mockrobiota
-class TestMockMethods(TestCase):
-
-    def setUp(self):
-        self.MOCK6_FORWARD_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-6/mock-forward-read.fastq.gz"
-        self.MOCK6_BARCODE_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-6/mock-index-read.fastq.gz"
-        self.MOCK6_DATASET_METADATA_URL = "https://raw.githubusercontent.com/caporaso-lab/mockrobiota/master/data/mock-6/dataset-metadata.tsv"
-
-        # Since mock-3 is small
-        self.MOCK3_FORWARD_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-3/mock-forward-read.fastq.gz"
-        self.MOCK3_BARCODE_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-3/mock-index-read.fastq.gz"
-        self.MOCK3_SAMPLE_METADATA_URL = "https://raw.githubusercontent.com/caporaso-lab/mockrobiota/master/data/mock-3/sample-metadata.tsv"
-
-    def test_get_dl_urls_pass(self):
-        with tempfile.TemporaryDirectory() as output_dir:
-            obs = get_dl_urls(self.MOCK6_DATASET_METADATA_URL, output_dir)
-
-        exp = [self.MOCK6_FORWARD_URL, self.MOCK6_BARCODE_URL]
-        for i, entry in enumerate(obs):
-            self.assertEqual(entry, exp[i])
-
-    def test_get_dl_urls_fail(self):
-        with self.assertRaises(Exception) as context:
-            get_dl_urls("asdf","asdf")
-
-    def test_download_mock_dataset(self):
-        with tempfile.TemporaryDirectory() as output_dir:
-            download_mock_dataset(output_dir, self.MOCK3_FORWARD_URL,
-                                  self.MOCK3_BARCODE_URL,
-                                  self.MOCK3_SAMPLE_METADATA_URL)
-            forward_fp = os.path.join(output_dir, "emp-single-end-sequences",
-                                      "sequences.fastq.gz")
-            self.assertTrue(os.path.exists(forward_fp))
-            self.assertTrue(os.path.getsize(forward_fp) > 0)
-
+#class TestMockMethods(TestCase):
+#
+#    def setUp(self):
+#        self.MOCK6_FORWARD_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-6/mock-forward-read.fastq.gz"
+#        self.MOCK6_BARCODE_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-6/mock-index-read.fastq.gz"
+#        self.MOCK6_DATASET_METADATA_URL = "https://raw.githubusercontent.com/caporaso-lab/mockrobiota/master/data/mock-6/dataset-metadata.tsv"
+#
+#        # Since mock-3 is small
+#        self.MOCK3_FORWARD_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-3/mock-forward-read.fastq.gz"
+#        self.MOCK3_BARCODE_URL = "https://s3-us-west-2.amazonaws.com/mockrobiota/latest/mock-3/mock-index-read.fastq.gz"
+#        self.MOCK3_SAMPLE_METADATA_URL = "https://raw.githubusercontent.com/caporaso-lab/mockrobiota/master/data/mock-3/sample-metadata.tsv"
+#
+#    def test_get_dl_urls_pass(self):
+#        with tempfile.TemporaryDirectory() as output_dir:
+#            obs = get_dl_urls(self.MOCK6_DATASET_METADATA_URL, output_dir)
+#
+#        exp = [self.MOCK6_FORWARD_URL, self.MOCK6_BARCODE_URL]
+#        for i, entry in enumerate(obs):
+#            self.assertEqual(entry, exp[i])
+#
+#    def test_get_dl_urls_fail(self):
+#        with self.assertRaises(Exception) as context:
+#            get_dl_urls("asdf","asdf")
+#
+#    def test_download_mock_dataset(self):
+#        with tempfile.TemporaryDirectory() as output_dir:
+#            download_mock_dataset(output_dir, self.MOCK3_FORWARD_URL,
+#                                  self.MOCK3_BARCODE_URL,
+#                                  self.MOCK3_SAMPLE_METADATA_URL)
+#            forward_fp = os.path.join(output_dir, "emp-single-end-sequences",
+#                                      "sequences.fastq.gz")
+#            self.assertTrue(os.path.exists(forward_fp))
+#            self.assertTrue(os.path.getsize(forward_fp) > 0)
+#
             barcode_fp = os.path.join(output_dir, "emp-single-end-sequences",
                                       "barcodes.fastq.gz")
             self.assertTrue(os.path.exists(barcode_fp))
