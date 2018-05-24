2ca2e68dada49252b53461a370aabd98c2c0b284
massoudTheDude <mahermassoud@gmail.com>
2018-05-24 14:25:17 -0700

    Script for single benchmark

--- dbt_ss_emp_single_pbs.sh
+++ dbt_ss_emp_single_pbs.sh
@@ -13,19 +13,16 @@
 
 #PBS -k oe
 
-
-fp_150nt="/projects/emp/03-otus/04-deblur/emp_deblur_150bp.release1.biom"
-fp_100nt="/projects/emp/03-otus/04-deblur/emp_deblur_100bp.release1.biom"
-fp_90nt="/projects/emp/03-otus/04-deblur/emp_deblur_90bp.release1.biom"
 i_dir="/panfs/panfs1.ucsd.edu/panscratch/mamaher/emp_ss_arr/ss1"
 
+source activate q2env
 cd $i_dir
 printf "identifier\truntime\n" > time_elapsed.tsv
-source activate q2env
 
 time sbiom -i emp_deblur_150bp_ss_10.biom \
            -i emp_deblur_100bp_ss_10.biom \
            -i emp_deblur_90bp_ss_10.biom  \
            -o . \
            -to time_elapsed.tsv \
-           -toa ss_10_arr-1
+           -toa ss_10_arr-1 \
+           -tl 150 -tl 100 -tl 90
