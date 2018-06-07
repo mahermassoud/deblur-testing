699bca9fad9ecb3bd6f75518d57d9f9c8673f3a0
mahermassoud <mahermassoud@gmail.com>
2018-06-07 11:44:13 -0700

    pbs scripts for checking ppn v runtime

--- ppn1_check_pr_parallel_post_trim_pbs.sh
+++ ppn1_check_pr_parallel_post_trim_pbs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=3
 
 #PBS -l walltime=00:30:00
 
 #PBS -l mem=10gb
 
-#PBS -N check pr_parallel_pt_ppn1
+#PBS -N check pr_parallel_pt_ppn3
 
 #PBS -m bea
 #PBS -M mahermassoud@gmail.com
@@ -15,14 +15,14 @@
 #PBS -t 1-3
 
 i_dir="/panfs/panfs1.ucsd.edu/panscratch/mamaher/check_parallel"
-o_name = "rand_100x100_arr${PBS_ARRRAY_ID}_ppn_1"
+o_name="rand_1000x1000_arr${PBS_ARRRAY_ID}_ppn_3"
 
 source activate q2env
 cd $i_dir
 
-time rpost_trim -i  rand_100x100.qza \
+time rpost_trim -i  rand_1000x1000.qza \
                 -o . \
                 -tl 100 \
                 -on $o_name \
-                -to times.tsv \
+                -to elapsed.tsv \
                 -toa $o_name
