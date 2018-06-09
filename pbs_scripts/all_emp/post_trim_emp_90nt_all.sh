8f5a9a8a83d14bbd78a694c00e794e507716df24
massoudTheDude <mahermassoud@gmail.com>
2018-06-08 18:30:23 -0700

    Scripts for post_trimming all of emp

--- pbs_scripts/all_emp/post_trim_emp_90nt_all.sh
+++ pbs_scripts/all_emp/post_trim_emp_90nt_all.sh
@@ -6,7 +6,7 @@
 
 #PBS -l mem=30gb
 
-#PBS -N post_trim_emp_100nt
+#PBS -N post_trim_emp_90nt
 
 #PBS -m bea
 #PBS -M mahermassoud@gmail.com
@@ -14,7 +14,7 @@
 #PBS -k oe
 
 fp_150nt="/projects/emp/03-otus/04-deblur/emp_deblur_150bp.release1.biom"
-i_dir="/panfs/panfs1.ucsd.edu/panscratch/mamaher/emp_all_out/100nt"
+i_dir="/panfs/panfs1.ucsd.edu/panscratch/mamaher/emp_all_out/90nt"
 o_name="emp_deblur_150bp_release1_pt_"
 
 source activate q2env
@@ -23,7 +23,7 @@ cd $i_dir
 
 time rpost_trim -ib $fp_100nt \
                 -o $o_dir\
-                -tl 100 \
+                -tl 90 \
                 -on $o_name \
                 -to elapsed.tsv \
                 -toa $o_name \
